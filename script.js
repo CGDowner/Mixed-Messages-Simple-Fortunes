fc19aca25a4d3b76b783ef03120cc67a64aa13c1
CGDowner <portfolio.connordowner@gmail.com>
2023-11-30 15:12:51 +0000

    Add Random Item Selection For Each Array

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 const msgPart1 = ['Today you will', 'Tommorow you will', 'In the near future you will'];
 const msgPart2 = [' face', ' be introduced to', ' come across' ];
 const msgPart3 = [' a noble hero.', ' an extreme evil.', ' a massive fortune.'];
+
+//Randomly Picks a Component From The First Array of Messages
+let randomMsgPart1 = msgPart1[Math.floor(Math.random()*msgPart1.length)];
+
+//Randomly Picks a Component From The Second Array of Messages
+let randomMsgPart2 = msgPart2[Math.floor(Math.random()*msgPart2.length)];
+
+//Randomly Picks a Component From The Third Array of Messages
+let randomMsgPart3 = msgPart3[Math.floor(Math.random()*msgPart3.length)];
