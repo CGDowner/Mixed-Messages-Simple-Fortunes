23745a7b14ccbc68b179fa57a9ecd3f09a05abe8
CGDowner <portfolio.connordowner@gmail.com>
2023-11-30 15:10:26 +0000

    Add msgPart Arrays

---
+++ script.js
@@ -0,0 +1,4 @@
+// Each Variable Holds An Array Holding Elements That Represent Message Components
+const msgPart1 = ['Today you will', 'Tommorow you will', 'In the near future you will'];
+const msgPart2 = [' face', ' be introduced to', ' come across' ];
+const msgPart3 = [' a noble hero.', ' an extreme evil.', ' a massive fortune.'];
